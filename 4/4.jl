968d4fe40cd27bbb52c1db6444cc4307b092f56b
bokwoon <bokwoon.c@gmail.com>
2019-01-03 00:49:31 +0800

    Holy shit this puzzle is harder than I thought
    Even after I had what I was confident was the correct answer, being off
    by 1 can result in a very huge difference from the actual answer due to
    the multiplication involved.

--- 4/4.jl
+++ 4/4.jl
@@ -1,8 +1,7 @@
-import DelimitedFiles
 import Dates
 import Dates.Date
 import Dates.DateTime
-strarray = readlines("4.in")
+import DelimitedFiles
 
 function sortbydate(strarray::Array{String,1})
     """
@@ -17,41 +16,85 @@ function sortbydate(strarray::Array{String,1})
         dict[dt] = str
     end
     strarraysorted = String[]
-    # sort(collect(dict)) changes Dict{DateTime,String} to a Array{Pair{DateTime,String}} sorted by DateTime
+    # sort(collect(dict)) changes Dict{DateTime,String} to a Array{Pair{DateTime,String}} sorted by the DateTime
     for pair in sort(collect(dict))
         push!(strarraysorted, pair[2])
     end
     strarraysorted
 end
-strarraysorted = sortbydate(strarray)
-DelimitedFiles.writedlm("4.out", strarraysorted, "\n")
 
-# Maybe a struct to hold the current status at each iteration would be a godsend here?
+mutable struct LoopStatus
+    """
+    Used to hold the current status at each iteration
+    """
+    id::Union{Int,Nothing}
+    sleep_start::Union{Int, Nothing}
+    sleep_end::Union{Int, Nothing}
+end
 
-let
-    dict = Dict{Int,Array{Int}}()
+function arraytodict(strarraysorted::Array{String})
+    """
+    Parses a string array and returns a dictionary Dict{Int,Array{Array{Int,1},1}}
+    of 'Guard ID' => 'Sleep/Wake schedule'
+    """
+    dict = Dict{Int,Array{Array{Int,1},1}}()
     schedule = zeros(Int, 60)
-    id = sleep_start = sleep_end = 0
-    firstloop = true
+    id = sleep_start = sleep_end = nothing
+    ls = LoopStatus(nothing, nothing, nothing)
     for str in strarraysorted
         dt = DateTime(match(r"^\[(?<dt>.+)\]", str)[:dt], "yyyy-mm-dd HH:MM")
         re = match(r"Guard #(?<id>\d+)", str)
         if re isa RegexMatch
-            if firstloop
-                id_curr = re[:id]
-                firstloop = false
+            id = tryparse(Int, re[:id])
+            if ls.id isa Nothing
+                ls.id = id
             else
-                id_prev = id_curr
-                id_curr = re[:id]
-                #> Insert entry for id_prev, schedule
-                    # I think date is not important to store because as long as the date differs we know we have to store a new schedule inside
+                if haskey(dict, ls.id)
+                    push!(dict[ls.id], schedule)
+                else
+                    dict[ls.id] = [schedule]
+                end
+                ls.id = id
+                ls.sleep_start = nothing
+                ls.sleep_end = nothing
             end
             schedule = zeros(Int, 60)
         elseif match(r"falls asleep", str) isa RegexMatch
-            sleep_start = Dates.minute(dt)
+            ls.sleep_start = Dates.minute(dt)
         elseif match(r"wakes up", str) isa RegexMatch
-            sleep_end = Dates.minute(dt)
-            schedule[sleep_start+1:sleep_end] .= 1
+            ls.sleep_end = Dates.minute(dt)
+            schedule[ls.sleep_start+1:ls.sleep_end] .= 1
         end
     end
+    dict
 end
+
+mutable struct Laziest
+    id::Int
+    hoursslept::Int
+    worstminute::Int
+end
+
+function main()
+    strarray = readlines("4.in")
+    strarraysorted = sortbydate(strarray)
+    DelimitedFiles.writedlm("4.out", strarraysorted, "\n")
+    dict = arraytodict(strarraysorted)
+    laziestguard = Laziest(-1, -1, -1)
+    for id in keys(dict)
+        array2D = hcat(dict[id]...)
+        array1D = reduce(+, array2D, dims=2)
+        hoursslept = reduce(+, reduce(+, array2D, dims=2), dims=1)[1,1]
+        worstminute = findmax(array1D, dims=1)[2][1,1][1] - 1
+        println("$id, $hoursslept, $worstminute")
+        if hoursslept > laziestguard.hoursslept
+            laziestguard.id = id
+            laziestguard.hoursslept = hoursslept
+            laziestguard.worstminute = worstminute
+        end
+    end
+    laziestguard.id,laziestguard.hoursslept,laziestguard.worstminute
+end
+#= sleep = max(reduce(+, hcat(dict[id]...), dims=2)...) =#
+
+isinteractive() || main()
